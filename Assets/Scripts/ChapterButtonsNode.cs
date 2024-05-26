15ef0590272664195bdc04cf29c4525053856951
Ellips <a.v.lemyakin@gmail.com>
2024-05-26 12:59:35 +0700

    add scroll value changing by button

--- Assets/Scripts/ChapterButtonsNode.cs
+++ Assets/Scripts/ChapterButtonsNode.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class ChapterButtonsNode : MonoBehaviour
@@ -10,8 +11,9 @@ public class ChapterButtonsNode : MonoBehaviour
     [SerializeField] private Transform buttonsContainer;
     [SerializeField] private ChapterButton buttonPrefab;
     [SerializeField] private ScrollView scrollView;
+    [SerializeField] private Scrollbar scrollbar;
 
-    private List<ChapterButton> _buttons;
+    private List<ChapterButton> _buttons = new();
 
     private void Start() 
     {
@@ -20,13 +22,15 @@ public class ChapterButtonsNode : MonoBehaviour
             var chapterButton = Instantiate(buttonPrefab, buttonsContainer);
             chapterButton.Init(chapter);
             chapterButton.OnClick += SwitchCurrentChapter;
+            _buttons.Add(chapterButton);
         });
+        SwitchCurrentChapter(_buttons[0]);
     }
 
     private void SwitchCurrentChapter(ChapterButton currentButton)
     {
         _buttons.ForEach(b => b.ChangeSelectStatus(true));
         currentButton.ChangeSelectStatus(false);
-        //scrollView
+        scrollbar.value = 1f - _buttons.IndexOf(currentButton) / (_buttons.Count-1f);
     }
 }
